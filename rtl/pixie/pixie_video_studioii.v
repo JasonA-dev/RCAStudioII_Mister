078a471e922e7def04595ad9225e2d40e0cfde61
JasonA <74770401+JasonA-dev@users.noreply.github.com>
2022-08-01 15:34:48 +0200

    further attempts to clean up the sync issues

--- rtl/pixie/pixie_video_studioii.v
+++ rtl/pixie/pixie_video_studioii.v
@@ -58,7 +58,7 @@ parameter  hsync_width_pixels = 12;
 parameter  lines_per_frame    = 262; // (constant for all 1861's) 
 parameter  active_v_lines     = 128; // (NTSC (PAL is 192. There is no PAL natively on StudioII))
 parameter  vsync_start_line   = 2;  
-parameter  vsync_height_lines = 12;  // 6
+parameter  vsync_height_lines = 6;  // 
 
 parameter  start_addr         = 'h0900;
 parameter  end_addr           = start_addr + 'hff;
@@ -147,9 +147,8 @@ reg [7:0] state = SM_LOAD_CACHE;
 reg [7:0] nbit;
 reg [7:0] byte_counter;
 reg load_byte = 1'b1;
-// 1. Read 8 bytes from vram into row cache
-always @(posedge clk) begin
-    if (enabled) begin
+always @(negedge clk) begin
+    if (enabled && video_de) begin
       case (state)
         SM_LOAD_CACHE:
           begin
@@ -158,7 +157,6 @@ always @(posedge clk) begin
                 vram_addr <= start_addr;
                 row_cache_counter <= 0;
                 row_cache_ready <= 1'b0;    
-                //$display("vram_addr == end_addr");                       
             end
             else begin
               // reset row cache counter if at 8
@@ -166,7 +164,6 @@ always @(posedge clk) begin
                 row_cache_counter <= 0;
                 row_cache_ready <= 1'b1;
                 mem_req <= 1'b0;  
-                //$display("row_cache_counter == 8");    
                 state <= SM_OUTPUT_VIDEO;                      
               end
               else begin
@@ -178,81 +175,79 @@ always @(posedge clk) begin
                   mem_addr <= vram_addr;
                   vram_addr <= vram_addr + 1;              
                   mem_req <= 1'b1;    
-                  //$display("vram into row cache: vram_addr %h data_in %h row_cache_counter %h", vram_addr, data_in, row_cache_counter);                        
                 end
               end
             end
           end   
         SM_OUTPUT_VIDEO:     
           begin
-            if(video_de) begin
-              //$display("SM_OUTPUT_VIDEO");
               if(load_byte == 1'b1) begin
                 // load a byte from row_cache into pixel_shift_reg
                 pixel_shift_reg <= row_cache[byte_counter];
-                load_byte = 1'b0;
-                //$display("pixel_shift_reg %h byte_counter %h", pixel_shift_reg, byte_counter);              
+                load_byte <= 1'b0;
                 end
               else begin
-                // process the byte to video
+                // output the bit to video
                 video <= pixel_shift_reg[7];   
-                pixel_shift_reg <= {pixel_shift_reg[6:0], 1'b0};                        
+                pixel_shift_reg <= pixel_shift_reg << 1;
 
+                // advance the bit & byte counters
                 nbit <= nbit + 1'd1;
-                // advance the byte counter
                 if (nbit == 8'd7) begin
                   nbit <= 8'd0;
-                  load_byte = 1'b1;  
+                  load_byte <= 1'b1;  
                   byte_counter <= byte_counter + 1'd1;              
                 end
 
+                // when all done return to load cache state  
                 if(byte_counter == 8'd7) begin
-                  // when all done return to load cache state  
                   byte_counter <= 8'd0;           
                   state <= SM_LOAD_CACHE;
                 end
-                $display("pixel_shift_reg %h video %h, byte_counter %h, load_byte %h, nbit %h", pixel_shift_reg, video, byte_counter, load_byte, nbit);
             end
           end
-        end
       endcase
     end
 end
 
-// 3. Repeat until all vram is read
-always @(posedge clk) begin
-end
-
-
 // Create HSync and HBlank
-always @(posedge clk) begin
+always @(negedge clk) begin
   if (horizontal_counter == (pixels_per_line-1)) begin
     new_h <= 1'd0;
     advance_v <= 1'b1;
   end
   else begin
-    new_h <= horizontal_counter + 1'd1;
+    new_h <= horizontal_counter + 1'd1;    // d1
   end
 
   horizontal_counter <= new_h;
   HSync <= (new_h < (hsync_start_pixel+hsync_width_pixels)) ? 1'b1 : 1'b0;  
-  HBlank <= (horizontal_counter < 18 || horizontal_counter > 82)  ? 1'b1 : 1'b0;  // 64 pixels wide
+  HBlank <= (new_h < 18 || new_h > 82)  ? 1'b1 : 1'b0;  // 64 pixels wide
+  //$display("horizontal_counter %d HSync %h HBlank %h", horizontal_counter, HSync, HBlank);
 end
 
 // Create VSync and VBlank
-always @(posedge clk) begin
+always @(negedge clk) begin
   if(advance_v) begin
-    advance_v <= 0;
+    advance_v <= 1'b0;
     if (vertical_counter==(lines_per_frame-1))
       new_v <= 1'd0;
     else begin
-      new_v <= vertical_counter + 1'b1;
+      new_v <= vertical_counter + 1'd1;      
     end
   end
 
   vertical_counter <= new_v;
   VSync <= (new_v < (vsync_start_line+vsync_height_lines)) ? 1'b1 : 1'b0;
-  VBlank <= (vertical_counter < 64 || vertical_counter > 192) ? 1'b1 : 1'b0;  // 128 lines for NTSC  
+  VBlank <= (new_v < 64 || new_v > 192) ? 1'b1 : 1'b0;  // 128 lines for NTSC  
+  //$display("vertical_counter %d VSync %h VBlank %h", vertical_counter, VSync, VBlank);
+end
+
+always @(negedge clk) begin
+    if(clk_enable) begin
+      EFx <= ((new_v >=60 && new_v < 65) || (new_v >= 188 && new_v < 193)) ? 1'b0 : 1'b1;  // OK
+      INT <= (new_v == 62)  ? 1'b1 : 1'b0;
+    end
 end
 
 endmodule
